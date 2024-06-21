39ed9a564426c884aa2fc993fe7f11ded7d1f7ba
Habibul Bashar Ahmed <Hahmed.y2k@gmail.com>
2024-06-21 20:59:42 +0530

    update_user Complete

--- src/app.py
+++ src/app.py
@@ -1,13 +1,7 @@
-from models.Users import User
-from utils.db import database_connection
-from sqlmodel import text
 from controllers.UserManager import UserManager
+from utils.db import database_connection
 
 if __name__ == '__main__':
     session = database_connection('postgresql')
-    # print("Connection successful")
-    # Use the User class from the users module
     user = UserManager(session)
-    user.create_user('Habib', 'hahmed.y2k@gmail.com', 'password', 'role')
-
-    #session.add(user)
+    user.update_user('4a696898-3f18-4c19-ab9b-963cddbd43c6', username='Habib Ahmed', email='hahmed.y2k@gmail.com', password='password123#', role='Student')
--- src/controllers/UserManager.py
+++ src/controllers/UserManager.py
@@ -1,15 +1,29 @@
 # Methods: create_user, authenticate, update_user, delete_user
 from sqlmodel import text
+
+
 class UserManager:
     def __init__(self, session):
         self._session = session
-        self.statement = None
 
-    def create_user(self, username, email, password, role):
-        self.statement = text(f"""INSERT INTO users (username, email, password, role) values ('{username}', '{email}', '{password}', '{role}')""")
-        self._session.exec(self.statement)
+    def create_user(self, username: str, email: str, password: str, role: str):
+        statement: text = text(
+            f"""INSERT INTO users (username, email, password, role) values ('{username}', '{email}', '{password}', '{role}')""")
+        self._session.exec(statement)
         self._session.commit()
-        return True
 
-    def update_user(self, user_id, *kwargs):
+    def update_user(self, user_id: str, **kwargs: str):
+        update_params: list = []
+
+        for key, value in kwargs.items():
+            update_string: str = f"{key}='{value}'"
+            update_params.append(update_string)
+
+        update_statement: str = ', '.join(update_params)
+
+        statement = text(f"""UPDATE users SET {update_statement} WHERE id='{user_id}'""")
+        self._session.exec(statement)
+        self._session.commit()
+
+    def delete_user(self):
         ...
